9226b4548fa326ad58862f54122b1ca38d8aa853
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-02-20 15:39:29 +0100

    State improvements

--- Interpreter/Interpreter/State.fs
+++ Interpreter/Interpreter/State.fs
@@ -12,18 +12,24 @@ module Interpreter.State
         | "if" | "then" | "else" | "while" | "declare" | "print" | "random" | "fork" | "__result__" -> true
         | _ -> false
 
-    // Checks if variable name v is valid
-    // - Valid if v starts with a letter or underscore
-    // - Valid if v contains only letters, numbers, or underscores
+    (* 
+       Checks if variable name v is valid
+       - Valid if v starts with a letter or underscore
+       - Valid if v contains only letters, numbers, or underscores
+    *)
     let validVariableName (v: string) = 
         if System.Char.IsAsciiLetter v[0] || v[0] = '_' then
             String.forall (function c -> System.Char.IsAsciiLetterOrDigit c || c = '_') v
         else    
             false
     
-    type state = unit // your type goes here
+    type state = 
+    | S of string
+    | X of int
+    | Empty of int
     
-    let mkState _ = failwith "not implemented"
+    let mkState = state.Empty
+
     let random _ = failwith "not implemented"
     
     let declare _ = failwith "not implemented"
