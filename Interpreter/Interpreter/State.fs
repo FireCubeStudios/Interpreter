6000b3e95f9801936aca899510ab4f4c6e1d328c
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-02-26 23:29:27 +0100

    Variables state environment

--- Interpreter/Interpreter/State.fs
+++ Interpreter/Interpreter/State.fs
@@ -1,16 +1,18 @@
 module Interpreter.State
     (*
-        Contains program state
+        This module Interpreter.State ontains program state
     *)
     open Result
     open Language
     
-    // Checks if variable name v is reserved
-    // Reserved names: if, then, else, while, declare, print, random, fork, or __result__
+    (*
+        Checks if variable name v is reserved
+        Reserved names: if, then, else, while, declare, print, random, fork, or __result__
+    *)
     let reservedVariableName v = 
         match v with
         | "if" | "then" | "else" | "while" | "declare" | "print" | "random" | "fork" | "__result__" -> true
-        | _ -> false
+        | _ -> false;;
 
     (* 
        Checks if variable name v is valid
@@ -21,21 +23,51 @@ module Interpreter.State
         if System.Char.IsAsciiLetter v[0] || v[0] = '_' then
             String.forall (function c -> System.Char.IsAsciiLetterOrDigit c || c = '_') v
         else    
-            false
+            false;;
     
-    type state = 
-    | S of string
-    | X of int
-    | Empty of int
+    (*
+        A "state" type record which contains a Map from strings to integers that contains the variables
+        aka basically a Map<VARIABLE_NAME, VARIABLE_VALUE>
+    *)
+    type state = { variables: Map<string, int> }
+
+    // A function that returns a state with an empty variable environment unit -> state
+    let mkState () = { variables = Map.empty }
     
-    let mkState = state.Empty
+    (*
+        A function to declare a variable 'x' with an initial value of 0
+        The function takes a variable name 'x' and a state 'st' and returns a "state option"
+        The "state option" returns "None" if any of the following conditions are violated:
+        - x is a valid variable name
+        - x is not a reserved variable name
+        - x does not exist in the variables in the state "st"
+        If all conditions are valid then a "state option" with the variable 'x' = 0 is returned
+    *)
+    let declare x st = 
+        if not(Map.containsKey x st.variables) && validVariableName x && not(reservedVariableName x) then
+            Some({ variables = Map.add x 0 st.variables })
+        else
+            None;;
+
+    (*
+        Given a variable name 'x' and a state 'st' return the value of 'x' if it exists in the state
+        If it does not exist in the state return None 
+        Otherwise return the value in a "Some" int option
+    *)
+    let getVar x st = Map.tryFind x st.variables
+
+    (*
+        Given a variable name 'x', an int value 'v' and a state 'st' set the value of 'x' to 'v'
+        If 'x' does not exist in the state return None 
+        Otherwise return the "state option" which has the variable 'x' updated to the value 'v'
+    *)
+    let setVar x v st =
+        if Map.containsKey x st.variables then
+            Some({ variables = Map.add x v st.variables })
+        else
+            None;;
 
     let random _ = failwith "not implemented"
-    
-    let declare _ = failwith "not implemented"
-    
-    let getVar _ = failwith "not implemented"
-    let setVar _ = failwith "not implemented"
     
     let push _ = failwith "not implemented"
     let pop _ = failwith "not implemented"     
