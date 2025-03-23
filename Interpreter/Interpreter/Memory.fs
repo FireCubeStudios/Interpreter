0d8b06353a4ae693018ed00012b5d41d6942dc0a
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-03-23 21:41:08 +0100

    Allocate memory

--- Interpreter/Interpreter/Memory.fs
+++ Interpreter/Interpreter/Memory.fs
@@ -1,14 +1,49 @@
 ﻿module Interpreter.Memory
-    
-    type memory = unit
+    (*
+        This module contains a model for memory
 
-    let empty _ = failwith "not implemented"
-
-    let alloc _ = failwith "not implemented"
+        The following piece of memory stores the values 2, 3, and 42 at addresses 0, 5, and 9, respectively
+         0 1 2 3 4 5 6 7 8  9 10 ...
+        -------------------------
+        |2| | | | |3| | | |42|  | ...
+        -------------------------
+    *)
     
+    (*
+        A "memory" type record which contains a Map from integers memory adresses) to integers (memory values)
+        Also contains an integer "next" which is a pointer to the next available free memory address
+        The "memory" map has memory addresses -> values 
+        Whenever we allocate memory the "next" pointer is increased (for simplicity it will never decrease)
+    *)
+    type memory = { memory: Map<int, int>; next: int }
+
+    (*
+        Creates a blank memory representation with the pointer "next" set to 0
+        NOTE: memSize argument unused right now
+    *)
+    let empty memSize = { memory = Map.empty; next = 0 }
+
+    (*
+        A function which that given an amount of memory to allocate "size" and a memory "mem" returns a (memory * int) option
+        It basically returns a newly allocated "memory" of amount "size" all set to 0 if "size" > 0
+        - Returns Some(mem', next) 
+            Where mem' is identical to "mem" but with all addresses from "next" to "next + size - 1" set to 0
+            Where the "next" pointer is updated to next + size
+        - Returns None otherwise if "size" is smaller than or equal to 0
+    *)
+    let alloc size mem = 
+        if size <= 0 then None 
+        else 
+            let allocatedMemory = 
+                List.fold (fun map address -> Map.add address 0 map) mem.memory [mem.next .. mem.next + size - 1] 
+            Some({ memory = allocatedMemory; next = mem.next }, mem.next + size)
+    
+
     let free _ = failwith "not implemented"
         
+
     let setMem _ = failwith "not implemented"
-        
+       
+       
     let getMem _ = failwith "not implemented"
 
--- Interpreter/Programs.fs
+++ Interpreter/Programs.fs
@@ -41,6 +41,9 @@ module Interpreter.Programs
                         While(Lt(Num 0, Var "x"),
                               Seq(Assign("result", Mul(Var "result", Var "y")),
                                   Assign("x", Var "x" .-. Num 1)))))))
+    
+    let (.++.) a b = Seq(a, b)
+
         
     let factorial_err3 x =
         Seq(Declare "result",
@@ -50,3 +53,98 @@ module Interpreter.Programs
                         While(Lt(Num 0, Var "x"),
                               Seq(Assign("result", Div(Var "result", Var "x" .-. Var "x")),
                                   Assign("x", Var "x" .-. Num 1)))))))
+
+    let factorialMem x =
+        Seq(Declare "ptr",
+            Seq(Alloc("ptr", Num x),
+                Seq(Declare "x",
+                    Seq(Assign("x", Num x),
+                        Seq(While(Lt(Num 0, Var "x"),
+                                  Seq(MemWrite(Var "ptr" .+. (Var "x" .-. Num 1), Var "x"),
+                                      Assign("x", Var "x" .-. Num 1))),
+                            Seq(Assign("x", Num (x - 1)),
+                                Seq(Declare "result",
+                                    Seq(Assign("result", Num 1),
+                                        Seq(While(Num 0 .<=. Var "x",
+                                                  Seq(Assign("result", Mul(Var "result", MemRead(Var "ptr" .+. Var "x"))),
+                                                      Assign("x", Var "x" .-. Num 1))),
+                                            Free(Var "ptr", Num x))))))))))
+    let factorialMem_err1 x =
+        Seq(Declare "ptr",
+            Seq(Alloc("ptr", Num (x - 1)),
+                Seq(Declare "x",
+                    Seq(Assign("x", Num x),
+                        Seq(While(Lt(Num 0, Var "x"),
+                                  Seq(MemWrite(Var "ptr" .+. (Var "x" .-. Num 1), Var "x"),
+                                      Assign("x", Var "x" .-. Num 1))),
+                            Seq(Assign("x", Num (x - 1)),
+                                Seq(Declare "result",
+                                    Seq(Assign("result", Num 1),
+                                        Seq(While(Num 0 .<=. Var "x",
+                                                  Seq(Assign("result", Mul(Var "result", MemRead(Var "ptr" .+. Var "x"))),
+                                                      Assign("x", Var "x" .-. Num 1))),
+                                            Free(Var "ptr", Num x))))))))))
+
+    let factorialMem_err2 x =
+        Seq(Declare "ptr",
+            Seq(Alloc("ptr", Num x),
+                Seq(Declare "x",
+                    Seq(Assign("x", Num x),
+                        Seq(While(Lt(Num 0, Var "x"),
+                                  Seq(MemWrite(Var "ptr" .+. (Var "x" .-. Num 1), Var "x"),
+                                      Assign("x", Var "x" .-. Num 1))),
+                            Seq(Assign("x", Num (x - 1)),
+                                Seq(Declare "result",
+                                    Seq(Assign("result", Num 1),
+                                        Seq(While(Num 0 .<=. Var "x",
+                                                  Seq(Assign("result", Mul(Var "result", MemRead(Var "ptr" .+. Var "x"))),
+                                                      Assign("x", Var "x" .-. Num 1))),
+                                            Free(Var "ptr", Num (x + 1)))))))))))
+
+    let factorialMem_err3 x =
+        Seq(Declare "ptr",
+            Seq(Alloc("ptr", Num x),
+                Seq(Declare "x",
+                    Seq(Assign("x", Num x),
+                        Seq(While(Lt(Num 0, Var "x"),
+                                  Seq(MemWrite(Var "ptr" .+. Var "x", Var "x"),
+                                      Assign("x", Var "x" .-. Num 1))),
+                            Seq(Assign("x", Num (x - 1)),
+                                Seq(Declare "result",
+                                    Seq(Assign("result", Num 1),
+                                        Seq(While(Num 0 .<=. Var "x",
+                                                  Seq(Assign("result", Mul(Var "result", MemRead(Var "ptr" .+. Var "x"))),
+                                                      Assign("x", Var "x" .-. Num 1))),
+                                            Free(Var "ptr", Num x))))))))))
+
+    let factorialMem_err4 x =
+        Seq(Declare "ptr",
+            Seq(Alloc("ptr", Num x),
+                Seq(Declare "x",
+                    Seq(Assign("x", Num x),
+                        Seq(While(Lt(Num 0, Var "x"),
+                                  Seq(MemWrite(Var "ptr" .+. (Var "x" .-. Num 1), Var "x"),
+                                      Assign("x", Var "x" .-. Num 1))),
+                            Seq(Assign("x", Num (x - 1)),
+                                Seq(Declare "result",
+                                    Seq(Assign("result", Num 1),
+                                        Seq(While(Num 0 .<=. Var "x",
+                                                  Seq(Assign("result", Mul(Var "result", MemRead(Var "ptr" .+. Var "x" .+. Num 1))),
+                                                      Assign("x", Var "x" .-. Num 1))),
+                                            Free(Var "ptr", Num x))))))))))
+                                            
+    let factorialMem_err5 x =
+        Seq(Declare "ptr",
+            Seq(Alloc("ptr", Num -x),
+                Seq(Declare "x",
+                    Seq(Assign("x", Num x),
+                        Seq(While(Lt(Num 0, Var "x"),
+                                  Seq(MemWrite(Var "ptr" .+. (Var "x" .-. Num 1), Var "x"),
+                                      Assign("x", Var "x" .-. Num 1))),
+                            Seq(Assign("x", Num (x - 1)),
+                                Seq(Declare "result",
+                                    Seq(Assign("result", Num 1),
+                                        Seq(While(Num 0 .<=. Var "x",
+                                                  Seq(Assign("result", Mul(Var "result", MemRead(Var "ptr" .+. Var "x" .+. Num 1))),
+                                                      Assign("x", Var "x" .-. Num 1))),
+                                            Free(Var "ptr", Num x))))))))))
