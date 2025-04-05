455d350d8ff28e01b50e7ca8d629bc120707fb94
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-04-05 16:56:54 +0200

    Comments in Memory.fs Memory.fsi

--- Interpreter/Interpreter/Memory.fs
+++ Interpreter/Interpreter/Memory.fs
@@ -20,17 +20,19 @@
 
     (*
         Creates a blank memory representation with the pointer "next" set to 0
-        NOTE: memSize argument unused right now
+        NOTE: memSize argument unused right now (red)
     *)
     let empty (memSize: int) = { memory = Map.empty; next = 0 }
 
     (*
-        A function that given an amount of memory to allocate "size" and a memory "mem" returns a (memory * int) option
+        A function that given an amount of memory to allocate "size" and a memory "mem" returns a (memory * int) result
         It basically returns a newly allocated "memory" of amount "size" all set to 0 if "size" > 0
-        - Returns Some(mem', next) 
+        - Returns Ok(mem', next) 
             Where mem' is identical to "mem" but with all addresses from "next" to "next + size - 1" set to 0
             Where the "mem.next" pointer is updated to next + size
-        - Returns None otherwise if "size" is smaller than or equal to 0
+        - Returns "Result.Error" of type:
+            NegativeMemoryAllocated (size) if "size" is smaller than or equal to 0
+            OutOfMemory if there is not "size" memory available in "mem" - TODO
     *)
     let alloc size mem = 
         if size <= 0 then Error (error.NegativeMemoryAllocated size) 
@@ -44,11 +46,14 @@
             Ok ({ memory = allocatedMemory; next = mem.next + size }, mem.next)
     
     (*
-        A function which takes a "ptr" pointer int, a "size" int and a "mem" memory type and returns a memory option
+        A function which takes a "ptr" pointer int, a "size" int and a "mem" memory type and returns a memory result
         This function basically frees up memory from "mem" starting at the pointer position "ptr"
-        - Returns Some (mem') where mem' is "mem", but all addresses from "ptr" to "ptr + size - 1" have been removed
+        - Returns Ok (mem') where mem' is "mem", but all addresses from "ptr" to "ptr + size - 1" have been removed
             As long as all of these addresses are allocated in "mem"
-        - Returns None otherwise, Note: the function does not decrease the "next" pointer
+        - Returns "Result.Error" of type MemoryNotAllocated (ptr') otherwise, 
+            if any memory in "mem" between "ptr" and "ptr + size - 1" is not allocated
+            where ptr' is the smallest address greater than or equal to "ptr" that is not allocated.
+            Note: the function does not decrease the "next" pointer
     *)
     let free ptr size mem = 
         let limit = ptr + size - 1
@@ -69,8 +74,8 @@
 
     (*
         A function to set a value 'v' at the address "ptr" in the "mem" memory
-        - returns a Some mem' option where mem' is the memory type with the value 'v' at the address "ptr"
-        - retrns a None option if the address "ptr" does not exist
+        - returns a Ok mem' result where mem' is the memory type with the value 'v' at the address "ptr"
+        - returns a "Result.Error" of type MemoryNotAllocated(ptr) if the address "ptr" does not exist
     *)
     let setMem ptr v mem = 
         if mem.memory.ContainsKey(ptr) then 
@@ -79,8 +84,8 @@
        
     (*
         A function to get the value from the address "ptr" in the "mem" memory
-        - returns a Some 'v' option  where 'v' is the value at the address "ptr" in the "mem" memory
-        - retrns a None option if the address "ptr" is not allocated
+        - returns a Ok 'v' result where 'v' is the value at the address "ptr" in the "mem" memory
+        - returns a "Result.Error" of type MemoryNotAllocated(ptr) if the address "ptr" is not allocated
     *)
     let getMem ptr mem = 
         match Map.tryFind ptr mem.memory with
