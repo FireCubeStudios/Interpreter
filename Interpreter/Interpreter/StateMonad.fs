74dda665d4e7e7c194b62657db09d6f2c39fc39d
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2025-04-06 20:25:06 +0200

    Added monad functions and signatures

--- Interpreter/Interpreter/StateMonad.fs
+++ Interpreter/Interpreter/StateMonad.fs
@@ -5,11 +5,62 @@
 
     type 'a stateMonad = SM of (state -> Result<'a * state, error>)
         
-    let ret x      = SM (fun st -> Ok(x, st))
-    let fail err= SM (fun _ -> Error err)
+    let ret x = SM (fun st -> Ok(x, st))
+    let fail err = SM (fun _ -> Error err)
     
     let bind (SM f) g =
         SM (fun st ->
             match f st with
             | Ok (x, st') -> let (SM h) = g x in h st'
             | Error err   -> Error err) 
+
+    (* Below are functions for the Monad *)
+
+    let declare str =
+        SM (fun st ->
+            match State.declare str st with
+            | Ok state -> Ok((), state)
+            | Error e -> Error e)
+
+    let setVar str v =
+        SM (fun st ->
+            match State.setVar str v st with
+            | Ok state -> Ok((), state)
+            | Error e -> Error e)
+
+    let getVar str =
+        SM (fun st ->
+            match State.getVar str st with
+            | Ok var -> Ok(var, st)
+            | Error e -> Error e)
+
+    let alloc str size =
+        SM (fun st ->
+            match State.alloc str size st with
+            | Ok state -> Ok((), state)
+            | Error e -> Error e)
+
+    let free ptr size =
+        SM (fun st ->
+            match State.free ptr size st with
+            | Ok state -> Ok((), state)
+            | Error e -> Error e)
+
+    let setMem ptr v =
+        SM (fun st ->
+            match State.setMem ptr v st with
+            | Ok state -> Ok((), state)
+            | Error e -> Error e)
+
+    let getMem ptr =
+        SM (fun st ->
+            match State.getMem ptr st with
+            | Ok memory -> Ok(memory, st)
+            | Error e -> Error e)
+
+    let random : int stateMonad = SM (fun st -> Ok (State.random st, st))
+
+    let evalState st a =
+        match a with
+        | Ok (v, state) -> Ok v
+        | Error e -> Error e;
--- Interpreter/Interpreter/StateMonad.fsi
+++ Interpreter/Interpreter/StateMonad.fsi
@@ -1,6 +1,7 @@
 ﻿module Interpreter.StateMonad
 
     open Language
+    open State
 
     type 'a stateMonad
     
@@ -13,3 +14,23 @@
 
     val (>>=) : 'a stateMonad -> ('a -> 'b stateMonad) -> 'b stateMonad
     val (>>>=) : 'a stateMonad -> 'b stateMonad -> 'b stateMonad
+
+    (* Below are functions for the Monad *)
+
+    val declare : string -> unit stateMonad
+
+    val setVar : string -> int -> unit stateMonad
+
+    val getVar : string -> int stateMonad
+
+    val alloc : string -> int -> unit stateMonad
+
+    val free : int -> int -> unit stateMonad
+
+    val setMem : int -> int -> unit stateMonad
+
+    val getMem : int -> int stateMonad
+
+    type random = int stateMonad
+
+    val evalState : state -> 'a stateMonad -> 'a option
